db5e69a6622212df0576d15e54e455055ae975d5
João Oliveira <joao.oliveira@elsanow.io>
2023-05-19 10:58:13 +0100

    added debug prints

--- preprocess_texts.py
+++ preprocess_texts.py
@@ -17,15 +17,17 @@ if __name__ == "__main__":
     parser.add_argument("--text_cleaners", nargs="+", default=["english_cleaners2"])
 
     args = parser.parse_args()
-
     for filelist in args.filelists:
         print("START:", filelist)
         filepaths_and_text = load_filepaths_and_text(filelist)
         for i in range(len(filepaths_and_text)):
             original_text = filepaths_and_text[i][args.text_index]
             cleaned_text = text._clean_text(original_text, args.text_cleaners)
+
             filepaths_and_text[i][args.text_index] = cleaned_text
+            print(cleaned_text)
 
         new_filelist = filelist + "." + args.out_extension
+        print(filepaths_and_text)
         with open(new_filelist, "w", encoding="utf-8") as f:
             f.writelines(["|".join(x) + "\n" for x in filepaths_and_text])
